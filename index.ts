e126496486101c28f52a666ef14baaf2b144eaca
ismailkhanz <ismailkhnaz@gmail.com>
2024-05-31 03:57:29 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! usr/bin/env node
+
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 10 +1)
+
+const answer = await inquirer.prompt([{
+    name :"userGuessedNumber",
+    type : "number",
+    message : "please guesse a number"
+}]);
+
+
+if(answer.userGuessedNumber === randomNumber){
+    console.log("Congratulaction you guessed correct number");
+}else{
+    console.log("you Guessed a wrong number");
+}
